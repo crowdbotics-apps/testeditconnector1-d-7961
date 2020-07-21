e677b7b47335d158e62e2646e5225932b0d01ef3
Crowdbotics <team@crowdbotics.com>
2020-07-21 14:27:44 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/testeditconnector1_d_7961/MainActivity.java
@@ -0,0 +1,15 @@
+package com.testeditconnector1_d_7961;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "testeditconnector1_d_7961";
+    }
+}
